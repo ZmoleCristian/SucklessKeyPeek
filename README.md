ca8fe231638ffb661fa8055c9996ca0a11bcbfef
tragdate <tragdate@gmail.com>
2023-06-17 18:37:48 +0300

    init

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
-# SucklessKeyPeek
- A bash-generated cheatsheet wallpaper for newcomers using my DWM fork, easing the learning curve until they master the Suckless experience.
+# SucklessKeyPeek 
+
+A bash-generated cheatsheet wallpaper for newcomers using my DWM fork, easing the learning curve until they master the Suckless experience. 
+
+## Features
+
+- Takes a custom wallpaper as background
+- Fills columns with text on top of background
+- Generates an output JPEG
+- Sets JPEG as wallpaper
+
+## Requirements
+
+- ImageMagick (must have `convert` installed)
+
+## Installation
+
+```bash
+git clone https://github.com/tragdate/SucklessKeyPeek
+```
+
+No build required. Place the script in the desired directory.
+
+## Usage
+
+1. Set an input image file in `generator.sh` or use provided `template_wood.png` 
+2. Set your parameters like `fontsize` `fontcolor` and more
+3. Add/Modify your cheatsheet content in the `text` file
+4. Run the script using the following command:
+
+```bash
+./generate.sh
+```
+
+4. The output image will be saved as `output.jpg` in the same directory
+
+## Parameteres
+
+- `cols`: number of columns
+- `margin`: margin for the columns
+- `fontsize`: font size for the text
+- `row_margin`: row margin
+- `fontcolor`: font color
+
+## Author
+
+🥷 [TragDate](https://tragdate.ninja)
+
+Documentation done by [docuTron](https://github.com/tragdate/docuTron)
+
+## License
+
+ISC
---
+++ generate.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+infile="template_wood.png"
+textfile="text"
+outfile="output.jpg"
+convert="/usr/bin/convert"
+
+cols=4
+margin=40
+fontsize=22
+row_margin=12
+fontcolor="white"
+
+img_dims=$(identify -format "%wx%h" "$infile")
+img_width=$(echo "$img_dims" | cut -dx -f1)
+img_height=$(echo "$img_dims" | cut -dx -f2)
+col_width=$(( (img_width - (cols + 1) * margin) / cols ))
+col_height=$(( img_height - 2 * margin ))
+
+# Copy input image to output
+cp "$infile" "$outfile"
+
+
+# Fill columns with text
+col=0
+line_height=$((fontsize + row_margin))
+y=$(( margin ))
+while read -r line; do
+    x=$(( margin + col * (col_width + margin) ))
+    if [[ $line == \#* ]]; then
+        line=${line#\#}
+        header_font=$((fontsize * 3 /2))
+            $convert "$outfile" -background none -fill "$fontcolor" -weight Bold -pointsize "$header_font" -size "${col_width}x${col_height}" -gravity NorthWest caption:"$line" -geometry +$x+$y -composite "$outfile"
+        else
+            $convert "$outfile" -background none -fill "$fontcolor" -weight Bold -pointsize "$fontsize" -size "${col_width}x${col_height}" -gravity NorthWest caption:"$line" -geometry +$x+$y -composite "$outfile"
+    fi
+    y=$((y + line_height))
+
+    # Move to next column when the bottom is reached
+    if ((y + line_height > img_height - margin)); then
+        y=$(( margin ))
+        col=$((col + 1))
+    fi
+
+    # Stop when reaching the last column
+    if (($((col / cols)) >= 1)); then
+        break
+    fi
+done < "$textfile"
+
+setbg output.jpg
+
